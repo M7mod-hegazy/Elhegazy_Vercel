bda996e6df4392254a55ddee5e41a2c2b2ba0fac
M7mod-hegazy <68657674+M7mod-hegazy@users.noreply.github.com>
2022-12-17 08:59:26 +0200

    false

--- frist/settings.py
+++ frist/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '57Eb060Ef463C7E9396850Adc95D2Ec3115B68174D72C468'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'elhegaze-v1.herokuapp.com/',
